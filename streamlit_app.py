bcb6b0c15dff6f218a614e46876e44fabd303b8b
SAYALI-008 <sayalium111@gmail.com>
2024-10-21 11:33:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,8 +22,7 @@ with st.expander('Data'):
   y_raw
 
 with st.expander('Data visualization'):
-  with st.expander('Scatter Plot'):
-    st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
+  st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
 
 
 # Sidebar for user interaction
