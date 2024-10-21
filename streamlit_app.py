f94e319da62732d1bc28c2e3fca74fb4f29ae0b0
SAYALI-008 <sayalium111@gmail.com>
2024-10-21 11:41:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,173 +1,68 @@
 import streamlit as st
-import numpy as np
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
-import pydeck as pdk
+import base64
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
 
-st.title('🤖 AppoML')
+st.title('NBA Player Stats Explorer')
 
-st.info('This is app builds a machine learning model for predicting the species of penguin')
+st.markdown("""
+This app performs simple webscraping of NBA player stats data!
+* **Python libraries:** base64, pandas, streamlit
+* **Data source:** [Basketball-reference.com](https://www.basketball-reference.com/).
+""")
 
-with st.expander('Data'):
-  st.write('**Raw data**')
-  df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
-  df
-
-  st.write('**X**')
-  X_raw = df.drop('species', axis=1)
-  X_raw
-
-  st.write('**y**')
-  y_raw = df.species
-  y_raw
-
-with st.expander('Data visualization'):
-  st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
-
-
-# Sidebar for user interaction
 st.sidebar.header('User Input Features')
+selected_year = st.sidebar.selectbox('Year', list(reversed(range(1950,2020))))
 
-penguins = df
-# Display the dataset
-st.header('Penguins Dataset')
-st.write(penguins.head())
+# Web scraping of NBA player stats
+@st.cache
+def load_data(year):
+    url = "https://www.basketball-reference.com/leagues/NBA_" + str(year) + "_per_game.html"
+    html = pd.read_html(url, header = 0)
+    df = html[0]
+    raw = df.drop(df[df.Age == 'Age'].index) # Deletes repeating headers in content
+    raw = raw.fillna(0)
+    playerstats = raw.drop(['Rk'], axis=1)
+    return playerstats
+playerstats = load_data(selected_year)
 
-# Display basic information about the dataset
-st.subheader('Basic Information')
-st.write(penguins.describe())
+# Sidebar - Team selection
+sorted_unique_team = sorted(playerstats.Tm.unique())
+selected_team = st.sidebar.multiselect('Team', sorted_unique_team, sorted_unique_team)
 
-# Show number of missing values
-st.subheader('Missing Values')
-st.write(penguins.isnull().sum())
+# Sidebar - Position selection
+unique_pos = ['C','PF','SF','PG','SG']
+selected_pos = st.sidebar.multiselect('Position', unique_pos, unique_pos)
 
-# Replace missing values (if needed)
-penguins.fillna(method='ffill', inplace=True)
+# Filtering data
+df_selected_team = playerstats[(playerstats.Tm.isin(selected_team)) & (playerstats.Pos.isin(selected_pos))]
 
-# Select features to visualize
-st.sidebar.subheader('Choose Features to Visualize')
-x_axis = st.sidebar.selectbox('Select X-axis Feature', penguins.columns, index=0)
-y_axis = st.sidebar.selectbox('Select Y-axis Feature', penguins.columns, index=1)
-hue_feature = st.sidebar.selectbox('Select Hue (Categorical)', ['species', 'island', 'sex'])
+st.header('Display Player Stats of Selected Team(s)')
+st.write('Data Dimension: ' + str(df_selected_team.shape[0]) + ' rows and ' + str(df_selected_team.shape[1]) + ' columns.')
+st.dataframe(df_selected_team)
 
-# # Scatterplot of the selected features
-# st.subheader(f'Scatterplot: {x_axis} vs {y_axis}')
-# fig = px.scatter(penguins, x=x_axis, y=y_axis, color=hue_feature, title=f'{x_axis} vs {y_axis}')
-# st.plotly_chart(fig)
+# Download NBA player stats data
+# https://discuss.streamlit.io/t/how-to-download-file-in-streamlit/1806
+def filedownload(df):
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()  # strings <-> bytes conversions
+    href = f'<a href="data:file/csv;base64,{b64}" download="playerstats.csv">Download CSV File</a>'
+    return href
 
-# Correlation heatmap
-st.subheader('Correlation Heatmap')
-corr_matrix = penguins.corr()
-fig, ax = plt.subplots()
-sns.heatmap(corr_matrix, annot=True, cmap='coolwarm', ax=ax)
-st.pyplot(fig)
+st.markdown(filedownload(df_selected_team), unsafe_allow_html=True)
 
-# # Histograms
-# st.subheader('Histograms')
-# feature_hist = st.sidebar.selectbox('Select Feature for Histogram', penguins.columns, index=3)
-# fig_hist = px.histogram(penguins, x=feature_hist, color=hue_feature, title=f'Histogram of {feature_hist}')
-# st.plotly_chart(fig_hist)
+# Heatmap
+if st.button('Intercorrelation Heatmap'):
+    st.header('Intercorrelation Matrix Heatmap')
+    df_selected_team.to_csv('output.csv',index=False)
+    df = pd.read_csv('output.csv')
 
-# Pairplot for multi-feature relationships
-st.subheader('Pairplot')
-if st.checkbox('Show Pairplot'):
-    st.write("Generating Pairplot...")
-    fig_pairplot = sns.pairplot(penguins, hue=hue_feature)
-    st.pyplot(fig_pairplot)
-  
-# Input features
-with st.sidebar:
-  st.header('Input features')
-  island = st.selectbox('Island', ('Biscoe', 'Dream', 'Torgersen'))
-  bill_length_mm = st.slider('Bill length (mm)', 32.1, 59.6, 43.9)
-  bill_depth_mm = st.slider('Bill depth (mm)', 13.1, 21.5, 17.2)
-  flipper_length_mm = st.slider('Flipper length (mm)', 172.0, 231.0, 201.0)
-  body_mass_g = st.slider('Body mass (g)', 2700.0, 6300.0, 4207.0)
-  gender = st.selectbox('Gender', ('male', 'female'))
-  
-  # Create a DataFrame for the input features
-  data = {'island': island,
-          'bill_length_mm': bill_length_mm,
-          'bill_depth_mm': bill_depth_mm,
-          'flipper_length_mm': flipper_length_mm,
-          'body_mass_g': body_mass_g,
-          'sex': gender}
-  input_df = pd.DataFrame(data, index=[0])
-  input_penguins = pd.concat([input_df, X_raw], axis=0)
-
-with st.expander('Input features'):
-  st.write('**Input penguin**')
-  input_df
-  st.write('**Combined penguins data**')
-  input_penguins
-
-
-# Data preparation
-# Encode X
-encode = ['island', 'sex']
-df_penguins = pd.get_dummies(input_penguins, prefix=encode)
-
-X = df_penguins[1:]
-input_row = df_penguins[:1]
-
-# Encode y
-target_mapper = {'Adelie': 0,
-                 'Chinstrap': 1,
-                 'Gentoo': 2}
-def target_encode(val):
-  return target_mapper[val]
-
-y = y_raw.apply(target_encode)
-
-with st.expander('Data preparation'):
-  st.write('**Encoded X (input penguin)**')
-  input_row
-  st.write('**Encoded y**')
-  y
-
-
-# Model training and inference
-## Train the ML model
-clf = RandomForestClassifier()
-clf.fit(X, y)
-
-## Apply model to make predictions
-prediction = clf.predict(input_row)
-prediction_proba = clf.predict_proba(input_row)
-
-df_prediction_proba = pd.DataFrame(prediction_proba)
-df_prediction_proba.columns = ['Adelie', 'Chinstrap', 'Gentoo']
-df_prediction_proba.rename(columns={0: 'Adelie',
-                                 1: 'Chinstrap',
-                                 2: 'Gentoo'})
-
-# Display predicted species
-st.subheader('Predicted Species')
-st.dataframe(df_prediction_proba,
-             column_config={
-               'Adelie': st.column_config.ProgressColumn(
-                 'Adelie',
-                 format='%f',
-                 width='medium',
-                 min_value=0,
-                 max_value=1
-               ),
-               'Chinstrap': st.column_config.ProgressColumn(
-                 'Chinstrap',
-                 format='%f',
-                 width='medium',
-                 min_value=0,
-                 max_value=1
-               ),
-               'Gentoo': st.column_config.ProgressColumn(
-                 'Gentoo',
-                 format='%f',
-                 width='medium',
-                 min_value=0,
-                 max_value=1
-               ),
-             }, hide_index=True)
-
-
-penguins_species = np.array(['Adelie', 'Chinstrap', 'Gentoo'])
-st.success(str(penguins_species[prediction][0]))
+    corr = df.corr()
+    mask = np.zeros_like(corr)
+    mask[np.triu_indices_from(mask)] = True
+    with sns.axes_style("white"):
+        f, ax = plt.subplots(figsize=(7, 5))
+        ax = sns.heatmap(corr, mask=mask, vmax=1, square=True)
+    st.pyplot()
